b6b7e4ee353c5bbc640058a88f0b14ac22077772
Vinnie Castro <vinniciusgrs@hotmail.com>
2025-11-03 10:45:12 -0700

    idk

--- Mutest/testing/simple_functions/simple_loop.py
+++ Mutest/testing/simple_functions/simple_loop.py
@@ -2,13 +2,14 @@
 Very simple loop function designed to demonstrate timeout mutations.
 """
 
+
 def count_to_five():
     """Count from 0 to 5 - very simple loop"""
     i = 0
     result = []
-    while i < 5:  # If < mutated to >, <=, or >= this could loop forever
+    while i < 5:
         result.append(i)
-        i = i + 1  # If + mutated to -, this loops forever
+        i = i - 1
     return result
 
 
@@ -16,7 +17,7 @@ def simple_sum(n):
     """Sum numbers from 1 to n"""
     total = 0
     i = 1
-    while i < n:  # If < mutated, may not terminate correctly
-        total = total + i  # If + mutated to -, wrong but won't timeout
-        i = i + 1  # If + mutated to -, infinite loop
+    while i < n:
+        total = total + i
+        i = i + 1
     return total
